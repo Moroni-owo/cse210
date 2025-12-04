42d09120ce505ca8350d59d91b9e27d250fff24f
Moroni Capcha <raulmoronicapchacadillo@gmail.com>
2025-12-04 09:14:46 -0500

    week6.2

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -1,9 +1,22 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Shapes Project.");
+        // Crear lista de shapes
+        List<Shape> shapes = new List<Shape>();
+
+        // Agregar formas
+        shapes.Add(new Square("Red", 5));
+        shapes.Add(new Rectangle("Blue", 4, 6));
+        shapes.Add(new Circle("Green", 3));
+
+        // Mostrar sus áreas
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine($"Color: {shape.GetColor()} - Area: {shape.GetArea()}");
+        }
     }
 }
