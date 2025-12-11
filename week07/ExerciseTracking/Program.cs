3ec753d1c7f58821b6923f7334529b94115242df
Moroni Capcha <raulmoronicapchacadillo@gmail.com>
2025-12-11 15:37:26 -0500

    week7

--- week07/ExerciseTracking/Program.cs
+++ week07/ExerciseTracking/Program.cs
@@ -1,9 +1,25 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the ExerciseTracking Project.");
+        List<Activity> activities = new List<Activity>();
+
+        // Crear una actividad de cada tipo
+        Running running = new Running("03 Nov 2022", 30, 4.8);
+        Cycling cycling = new Cycling("03 Nov 2022", 45, 20.0);
+        Swimming swimming = new Swimming("03 Nov 2022", 25, 40);
+
+        activities.Add(running);
+        activities.Add(cycling);
+        activities.Add(swimming);
+
+        // Mostrar los summaries
+        foreach (Activity activity in activities)
+        {
+            Console.WriteLine(activity.GetSummary());
+        }
     }
 }
